5b5c4b9ae34c18df94042811a750ec038dd95435
xcfstudio <xcf2370@outlook.com>
2026-07-07 14:28:02 +0800

    update footer

--- src/components/FooterContent.vue
+++ src/components/FooterContent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="footer-content">Copyright © 2026 Link-UI 版权所有</div>
+  <div class="footer-content">Copyright © 2026 Link-UI 版权所有 | <i class="iconfont">&#xe886;</i><a href="https://github.com/xcfstudio/LinkUI"> LinkUI Project</a> </div>
 </template>
 
 <style scoped lang="scss">
